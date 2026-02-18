0698c3590c416788bc3893b4b43e52b812430de1
RangaReddy12 <pranga2010@gmail.com>
2026-02-18 12:00:20 +0530

    FirstTestcase

---
+++ src/test/java/feb18/Demo.java
@@ -0,0 +1,20 @@
+package feb18;
+
+public class Demo {
+public void login()
+{
+	System.out.println("Login");
+}
+public void customer()
+{
+	System.out.println("customer");
+}
+public void supplier()
+{
+	System.out.println("supplier");
+}
+public void stocitems()
+{
+	System.out.println("stockitems");
+}
+}
