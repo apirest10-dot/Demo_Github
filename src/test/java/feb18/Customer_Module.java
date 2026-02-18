40803a73c5d0c5ab7d96456be64badf97ada07bb
RangaReddy12 <pranga2010@gmail.com>
2026-02-18 12:12:27 +0530

    new Test

---
+++ src/test/java/feb18/Customer_Module.java
@@ -0,0 +1,26 @@
+package feb18;
+
+public class Customer_Module {
+	public void login()
+	{
+		System.out.println("Login");
+	}
+	public void customer()
+	{
+		System.out.println("customer");
+	}
+	public void supplier()
+	{
+		System.out.println("supplier");
+	}
+	public void stocitems()
+	{
+		System.out.println("stockitems");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
